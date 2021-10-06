29d1c9320674c01d987b4425ef56a30749f17d30
Cris-0001 <cristhian0001@hotmail.com>
2021-10-05 19:43:01 -0500

    frontend-arbitro-detalles-eliminar

--- Aplicacion/Frontend/Pages/Arbitro/ArbDetails.cshtml.cs
+++ Aplicacion/Frontend/Pages/Arbitro/ArbDetails.cshtml.cs
@@ -4,13 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Dominio;
+using Persistencia;
 
 namespace Frontend.Pages
 {
     public class ArbDetailsModel : PageModel
     {
-        public void OnGet()
+        //Referenciar el repositorio
+        private readonly IRepositorioArbitro repoArbitro;
+        
+        //Constructor
+        public ArbDetailsModel(IRepositorioArbitro _repoArbitro){
+            this.repoArbitro=_repoArbitro;
+        }
+
+        //Propiedad para comunicacion con el cshtml
+        [BindProperty]
+        public Arbitro Arbitro{get;set;}
+
+        public ActionResult OnGet(int id)
         {
+            Arbitro=repoArbitro.BuscarArbitro(id);
+            return Page();
         }
     }
 }
